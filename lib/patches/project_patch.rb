5055aedb8bda1f1dd0e7d3fbdf762a81f31cd6fe
bilel-kedidi <bilel.kedidi@gmail.com>
2019-02-20 10:58:22 +0100

    compatible 3.4

--- lib/patches/project_patch.rb
+++ lib/patches/project_patch.rb
@@ -8,12 +8,14 @@ module  Patches
       base.send(:include, InstanceMethods)
       base.class_eval do
         # if Setting.send("plugin_redmine_enhanced_projects_list")['sorting_name'][8..-1]
-          acts_as_nested_set left_column: :order_lft,
-                             right_column: :order_rgt,
-                             :order_column => (Setting.send("plugin_redmine_enhanced_projects_list")['sorting_name'][8..-1] ? Setting.send("plugin_redmine_enhanced_projects_list")['sorting_name'][8..-1].to_sym : :custom_value_content)
+        acts_as_nested_set left_column: :order_lft,
+                           right_column: :order_rgt,
+                           :order_column => (Setting.send("plugin_redmine_enhanced_projects_list")['sorting_name'][8..-1] ? Setting.send("plugin_redmine_enhanced_projects_list")['sorting_name'][8..-1].to_sym : :custom_value_content)
         # end
         class<< self
-          alias_method_chain :project_tree, :new_order
+          alias_method_chain :project_tree, :new_order_old unless Redmine::VERSION.to_s > '3.4.0'
+          alias_method_chain :project_tree, :new_order if Redmine::VERSION.to_s > '3.4.0'
+
           # alias_method_chain :next_identifier, :year
 
           def change_act_as_nested
@@ -38,8 +40,22 @@ module  Patches
 
 
   module ClassMethods
-    def project_tree_with_new_order(projects, &block)
-      return project_tree_without_new_order(projects, &block) if projects.blank? or  !projects.first.attributes.has_key?('order_lft') or !projects.first.attributes.has_key?( 'order_rgt' )
+    def project_tree_with_new_order(projects, options={}, &block)
+      return project_tree_without_new_order(projects, options, &block) if projects.blank? or  !projects.first.attributes.has_key?('order_lft') or !projects.first.attributes.has_key?( 'order_rgt' )
+      ancestors = []
+      # projects = get_all_projects(projects, order_desc, is_closed)
+      projects = Project.get_new_tree(projects)
+      projects.each do |project|
+        while ancestors.any? && !project.is_descendant_of?(ancestors.last)
+          ancestors.pop
+        end
+        yield project, ancestors.size
+        ancestors << project
+      end
+    end
+
+    def project_tree_with_new_order_old(projects, &block)
+      return project_tree_without_new_order_old(projects, &block) if projects.blank? or  !projects.first.attributes.has_key?('order_lft') or !projects.first.attributes.has_key?( 'order_rgt' )
       ancestors = []
       # projects = get_all_projects(projects, order_desc, is_closed)
       projects = Project.get_new_tree(projects)
